541cef7839de3202094f679d0adf51107e1506d2
John Francis <jean_f_05@hotmail.com>
2021-12-24 13:06:44 +0000

    nodemail sender tweak

--- utils/nodemailer.js
+++ utils/nodemailer.js
@@ -3,10 +3,10 @@ const nodemailer = require("nodemailer");
 const sendEmail = async (receiver, subject, text) => {
   try {
     const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
+      // host: "smtp.gmail.com",
       service: "gmail",
-      port: 587,
-      secure: false,
+      // port: 587,
+      // secure: false,
       auth: {
         user: process.env.MAIL_USERNAME,
         pass: process.env.MAIL_PASSWORD,
