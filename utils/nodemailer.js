9150283b174d1f2ee2cd0e4cfa8cae84b09c2c75
John Francis <jean_f_05@hotmail.com>
2021-12-24 13:01:41 +0000

    fixed nodemail sender

--- utils/nodemailer.js
+++ utils/nodemailer.js
@@ -1,29 +1,29 @@
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 
-const sendEmail = async (receiver,subject,text) =>{
-try {
-
+const sendEmail = async (receiver, subject, text) => {
+  try {
     const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com", 
+      host: "smtp.gmail.com",
       service: "gmail",
-        port:587,
-        secure:true,
-        auth: {
-          user: process.env.MAIL_USERNAME,
-          pass: process.env.MAIL_PASSWORD
-        },
-      });
+      port: 587,
+      secure: false,
+      auth: {
+        user: process.env.MAIL_USERNAME,
+        pass: process.env.MAIL_PASSWORD,
+      },
+    });
 
-      await transporter.sendMail({
-        from: process.env.USER,
-        to: receiver,
-        subject: subject,
-        text: text,
-        });
+    await transporter.sendMail({
+      from: "noreplycali00@gmail.com",
+      to: receiver,
+      subject: subject,
+      text: text,
+    });
 
-        console.log("EMAIL SUCCESSFULLY SENT!");
-} catch (error) {
+    console.log("EMAIL SUCCESSFULLY SENT!");
+  } catch (error) {
     console.log(error, "FAILED SENDING EMAIL...");
-}};
+  }
+};
 
 module.exports = sendEmail;
